4c153535352abe6fc2690e87b2d77537505e3a33
Quentin PAYET <quentinpayet1203@gmail.com>
2023-09-08 14:40:33 +0400

    wip: cleaning training.ipynb with notebook_fn.py and notebook_var.py

--- labos/notebook_fn.py
+++ labos/notebook_fn.py
@@ -1,6 +1,14 @@
 import torch
 from torch import nn
 
+from sklearn.metrics import (
+    confusion_matrix,
+    ConfusionMatrixDisplay,
+    classification_report,
+    precision_score,
+    recall_score,
+    f1_score,
+)
 from sklearn.model_selection import train_test_split
 
 import pandas as pd
@@ -177,3 +185,161 @@ def get_data(df: pd.DataFrame, device: str):
         X, y, test_size=0.2, random_state=42
     )
     return X_train, X_test, y_train, y_test
+
+
+def training_loop(
+    MusicClassifier: MusicClassifier,
+    df: pd.DataFrame,
+):
+    # Device agnostic code
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    # Init model
+    model = init_model(MusicClassifier).to(device)
+
+    loss_fn, optimizer = get_training_fns(model)
+
+    # Prepare data
+    X_train, X_test, y_train, y_test = get_data(df, device)
+
+    precision = {}
+    recall = {}
+    f1_score_values = {}
+
+    # Training loop
+    torch.manual_seed(42)
+    epochs = 126
+
+    loss_history = []
+    acc_history = []
+    test_loss_history = []
+    test_acc_history = []
+
+    for epoch in range(epochs):
+        """
+        Train
+        """
+        model.train()
+
+        # 1. Forward pass
+        y_logits = model(X_train)
+        y_pred = torch.softmax(y_logits, dim=1).argmax(dim=1)
+
+        # 2. Metrics
+        loss = loss_fn(y_logits, y_train)
+        acc = accuracy_fn(y_true=y_train, y_pred=y_pred)
+
+        # 2.1 Save metrics
+        loss_history.append(loss.cpu().detach().numpy())
+        acc_history.append(acc)
+
+        # 3. Zero Grad
+        optimizer.zero_grad()
+
+        # 4. Backpropagation
+        loss.backward()
+
+        # 5. Optimmizer step
+        optimizer.step()
+
+        """
+        Test
+        """
+        model.eval()
+
+        with torch.inference_mode():
+            # 1. Forward pass
+            y_test_logits = model(X_test)
+            y_test_pred = torch.softmax(y_test_logits, dim=1).argmax(dim=1)
+
+            # 2. Metrics
+            test_loss = loss_fn(y_test_logits, y_test)
+            test_acc = accuracy_fn(y_pred=y_test_pred, y_true=y_test)
+
+            # 2.1 Save metrics
+            test_loss_history.append(test_loss.cpu().detach().numpy())
+            test_acc_history.append(test_acc)
+
+        # Print out what's happening
+        if epoch == 125:
+            print(
+                f"Epoch: {epoch} | Loss: {loss:.5f}, Acc: {acc:.2f}% | Test Loss: {test_loss:.5f}, Test Acc: {test_acc:.2f}%"
+            )
+
+    # TODO: Factoriser
+    # Matrice de confusion
+    ConfusionMatrixDisplay(
+        confusion_matrix(y_test.cpu(), y_test_pred.cpu().numpy())
+    ).plot()
+
+    # Metrics d'entrainements
+    display_training_metrics(
+        epochs, loss_history, test_loss_history, acc_history, test_acc_history
+    )
+
+    # Métrics supplémentaires
+    print(classification_report(y_true=y_test.cpu(), y_pred=y_test_pred.cpu()))
+
+    # TODO: Factoriser
+    for i in range(10):
+        # Per class
+        precision[i] = precision_score(
+            y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average=None
+        )[
+            i
+        ]  # TODO: Check what is the good value for 'average'
+        recall[i] = recall_score(
+            y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average=None
+        )[i]
+        f1_score_values[i] = f1_score(
+            y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average=None
+        )[i]
+
+    # Global
+    precision_micro = precision_score(
+        y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average="micro"
+    )
+    precision_macro = precision_score(
+        y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average="macro"
+    )
+    precision_weighted = precision_score(
+        y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average="weighted"
+    )
+
+    recall_micro = recall_score(
+        y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average="micro"
+    )
+    recall_macro = recall_score(
+        y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average="macro"
+    )
+    recall_weighted = recall_score(
+        y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average="weighted"
+    )
+
+    f1_score_micro = f1_score(
+        y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average="micro"
+    )
+    f1_score_macro = f1_score(
+        y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average="macro"
+    )
+    f1_score_weighted = f1_score(
+        y_true=y_test.cpu(), y_pred=y_test_pred.cpu(), average="weighted"
+    )
+    # ! Faire pareil pour recall et f1_score !
+    # TODO: Retourner somme et détails de precision, recall et f1_score pour plots les évolutions !
+    # ! Utiliser sklearn.metrics.precision etc...
+    return (
+        test_acc,
+        precision,
+        recall,
+        f1_score_values,
+        precision_micro,
+        precision_macro,
+        precision_weighted,
+        recall_micro,
+        recall_macro,
+        recall_weighted,
+        f1_score_micro,
+        f1_score_macro,
+        f1_score_weighted,
+    )
