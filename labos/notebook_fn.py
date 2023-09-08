15a56e194083b66d17cd5677f138a8a259b74462
Quentin PAYET <quentinpayet1203@gmail.com>
2023-09-08 14:23:14 +0400

    wip: cleaning training.ipynb with notebook_fn.py and notebook_var.py

--- labos/notebook_fn.py
+++ labos/notebook_fn.py
@@ -1,9 +1,15 @@
 import torch
 from torch import nn
 
+from sklearn.model_selection import train_test_split
+
 import pandas as pd
+import matplotlib.pyplot as plt
+
+from typing import List
 
 # TODO: Add type to everything
+# TODO: Ranger avec bloc de commentaires OU plusieurs modules (pred, visualisation, ...)
 
 
 class MusicClassifier(nn.Module):
@@ -76,3 +82,98 @@ def predict(model: MusicClassifier, df: pd.DataFrame, genre_mapping: dict[int, s
             actual_best = class_predictions.count(elt)
             prediction = elt
     return prediction, raw_results
+
+
+def filter_data(
+    kept_df: pd.DataFrame,
+    threshold: float,
+    raw_results: list,
+    dataframe: pd.DataFrame,
+    real_class: int,
+    column_names: List[str],
+):
+    for i in range(len(raw_results)):
+        if raw_results[i][0][int(real_class)] > threshold:
+            kept_df = pd.concat(
+                [
+                    kept_df,
+                    pd.DataFrame(
+                        dataframe.iloc[i].to_numpy().reshape(55, 1).transpose(),
+                        columns=column_names,
+                    ),
+                ],
+                axis=0,
+            )
+            kept_df["label"] = real_class
+    return kept_df
+
+
+def display_training_metrics(
+    epochs: int,
+    loss_history: List[float],
+    test_loss_history: List[float],
+    acc_history: List[float],
+    test_acc_history: List[float],
+):
+    plt.figure(figsize=(15, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(range(epochs), loss_history, label="Training loss")
+    plt.plot(range(epochs), test_loss_history, label="Testing loss")
+    plt.legend()
+    plt.title("Loss")
+
+    plt.subplot(1, 2, 2)
+    plt.plot(range(epochs), acc_history, label="Training acc")
+    plt.plot(range(epochs), test_acc_history, label="Testing acc")
+    plt.legend()
+    plt.title("Accuracy")
+
+    plt.show()
+
+
+def plot_accuracy_evolution(values: List[float]):
+    # TODO: Refactor
+    plt.plot(values)
+    plt.title("Accuracy evolution through trainings")
+    plt.xlabel("Trainings")
+    plt.ylabel("Accuracy")
+    plt.show()
+
+
+def plot_metrics_evolution(metrics_history: dict, metrics_name: str):
+    plt.figure(figsize=(15, 5))
+    for i in range(10):
+        plt.plot(metrics_history[i], label=f"{metrics_name} class {i}")
+    plt.title(f"Class {metrics_name} evolutions through trainings")
+    plt.xlabel("Trainings")
+    plt.ylabel(metrics_name)
+    plt.legend()
+    plt.show()
+
+
+def plot_global_metrics_evolution(
+    micro_history, macro_history, weighted_history, metric_name: str
+):
+    plt.figure(figsize=(15, 5))
+    plt.plot(micro_history, label="micro")
+    plt.plot(macro_history, label="macro")
+    plt.plot(weighted_history, label="weighted")
+    plt.title(f"Mean {metric_name} through trainings")
+    plt.legend()
+    plt.show()
+
+
+def get_data(df: pd.DataFrame, device: str):
+    # TODO: Séparer avec une nouvelle fonction get_test_data !
+    # Prepare data
+    X = (
+        torch.from_numpy(df.drop(columns=["label"]).to_numpy())
+        .type(torch.float32)
+        .to(device)
+    )
+    y = torch.from_numpy(df["label"].to_numpy()).type(torch.long).to(device)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+    return X_train, X_test, y_train, y_test
