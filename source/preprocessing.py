cf99a56813e4a8fc9de0362fdeff77dec1a66188
Quentin <quentinpayet1203@gmail.com>
2024-05-09 22:22:53 +0400

    wip: cleaning preprocessing.py

--- source/preprocessing.py
+++ source/preprocessing.py
@@ -1,95 +1,11 @@
-import io
-import librosa
-import numpy as np
-import pandas as pd
-
 import joblib
+import pandas as pd
 from typing import List
 
-from . import var
-from .var import PreprocessingType
+from .var import PreprocessingType, column_names
+from .utils import preprocessing_utils
 
 # TODO: Vérifier les typages
-
-
-def get_3sec_sample(uploaded_audio: bytes) -> List[np.ndarray]:
-    audio, sample_rate = librosa.load(
-        io.BytesIO(uploaded_audio),
-        sr=None,
-    )
-
-    segment_duration = 3  # Durée de chaque segment en secondes
-    segment_length = int(sample_rate * segment_duration)
-    segments = []
-
-    # Découpage
-    for i in range(0, len(audio), segment_length):
-        segment = audio[i : i + segment_length]
-        segments.append(segment)
-
-    return segments
-
-
-def audio_pipeline(audio: np.ndarray) -> List[float]:
-    features = []
-
-    # Chromagram
-    chroma_stft = librosa.feature.chroma_stft(y=audio)
-    features.append(np.mean(chroma_stft))
-    features.append(np.var(chroma_stft))  # var => variance
-
-    # RMS (Root Mean Square value for each frame)
-    rms = librosa.feature.rms(y=audio)
-    features.append(np.mean(rms))
-    features.append(np.var(rms))
-
-    # Calcul du Spectral centroid
-    spectral_centroids = librosa.feature.spectral_centroid(y=audio)
-    features.append(np.mean(spectral_centroids))
-    features.append(np.var(spectral_centroids))
-
-    # Spectral bandwith
-    spectral_bandwidth = librosa.feature.spectral_bandwidth(y=audio)
-    features.append(np.mean(spectral_bandwidth))
-    features.append(np.var(spectral_bandwidth))
-
-    # Calcul du spectral rolloff point
-    rolloff = librosa.feature.spectral_rolloff(y=audio)
-    features.append(np.mean(rolloff))
-    features.append(np.var(rolloff))
-
-    # Calcul du ZCR (Zero Crossing Rate)
-    zcr = librosa.zero_crossings(audio)
-    # features.append(np.sum(zcr))  # Custom
-    features.append(np.mean(zcr))
-    features.append(np.var(zcr))
-
-    # Harmonic
-    harmony = librosa.effects.harmonic(y=audio)
-    features.append(np.mean(harmony))
-    features.append(np.var(harmony))
-
-    # Tempo
-    tempo = librosa.feature.tempo(y=audio)
-    features.append(tempo[0])
-
-    # Calcul des moyennes des MFCC
-    mfcc = librosa.feature.mfcc(y=audio)
-    for x in mfcc:
-        features.append(np.mean(x))
-        features.append(np.var(x))
-
-    return features
-
-
-def get_30_percent(segments: List[np.ndarray]) -> List[np.ndarray]:
-    lighten_segments: List[np.ndarray] = []
-
-    for i in range(len(segments)):
-        if i % 3 == 0:
-            lighten_segments.append(segments[i])
-
-    return lighten_segments
 
 
 def preprocess_data(
@@ -97,20 +13,20 @@ def preprocess_data(
 ) -> List[pd.DataFrame]:
     scaler = joblib.load(scaler_path)
     dfs = []
-    segments = get_3sec_sample(uploaded_audio)
+    segments = preprocessing_utils.get_3sec_sample(uploaded_audio)
 
     if preprocessing_type == PreprocessingType.fast:
-        segments = get_30_percent(segments)
+        segments = preprocessing_utils.get_30_percent(segments)
 
     for audio in segments:
         # Perform audio feature extraction
-        features = audio_pipeline(audio)
+        features = preprocessing_utils.audio_pipeline(audio)
 
         # Scale the features using the loaded scaler
         scaled_features = scaler.transform([features])
 
         # Create a DataFrame
-        df = pd.DataFrame(scaled_features, columns=var.column_names)
+        df = pd.DataFrame(scaled_features, columns=column_names)
         dfs.append(df)
 
     return dfs
---
+++ source/utils/preprocessing_utils.py
@@ -0,0 +1,85 @@
+import io
+import librosa
+import numpy as np
+
+from typing import List
+
+
+def get_3sec_sample(uploaded_audio: bytes) -> List[np.ndarray]:
+    audio, sample_rate = librosa.load(
+        io.BytesIO(uploaded_audio),
+        sr=None,
+    )
+
+    segment_duration = 3  # Durée de chaque segment en secondes
+    segment_length = int(sample_rate * segment_duration)
+    segments = []
+
+    # Découpage
+    for i in range(0, len(audio), segment_length):
+        segment = audio[i : i + segment_length]
+        segments.append(segment)
+
+    return segments
+
+
+def audio_pipeline(audio: np.ndarray) -> List[float]:
+    features = []
+
+    # Chromagram
+    chroma_stft = librosa.feature.chroma_stft(y=audio)
+    features.append(np.mean(chroma_stft))
+    features.append(np.var(chroma_stft))  # var => variance
+
+    # RMS (Root Mean Square value for each frame)
+    rms = librosa.feature.rms(y=audio)
+    features.append(np.mean(rms))
+    features.append(np.var(rms))
+
+    # Calcul du Spectral centroid
+    spectral_centroids = librosa.feature.spectral_centroid(y=audio)
+    features.append(np.mean(spectral_centroids))
+    features.append(np.var(spectral_centroids))
+
+    # Spectral bandwith
+    spectral_bandwidth = librosa.feature.spectral_bandwidth(y=audio)
+    features.append(np.mean(spectral_bandwidth))
+    features.append(np.var(spectral_bandwidth))
+
+    # Calcul du spectral rolloff point
+    rolloff = librosa.feature.spectral_rolloff(y=audio)
+    features.append(np.mean(rolloff))
+    features.append(np.var(rolloff))
+
+    # Calcul du ZCR (Zero Crossing Rate)
+    zcr = librosa.zero_crossings(audio)
+    # features.append(np.sum(zcr))  # Custom
+    features.append(np.mean(zcr))
+    features.append(np.var(zcr))
+
+    # Harmonic
+    harmony = librosa.effects.harmonic(y=audio)
+    features.append(np.mean(harmony))
+    features.append(np.var(harmony))
+
+    # Tempo
+    tempo = librosa.feature.tempo(y=audio)
+    features.append(tempo[0])
+
+    # Calcul des moyennes des MFCC
+    mfcc = librosa.feature.mfcc(y=audio)
+    for x in mfcc:
+        features.append(np.mean(x))
+        features.append(np.var(x))
+
+    return features
+
+
+def get_30_percent(segments: List[np.ndarray]) -> List[np.ndarray]:
+    lighten_segments: List[np.ndarray] = []
+
+    for i in range(len(segments)):
+        if i % 3 == 0:
+            lighten_segments.append(segments[i])
+
+    return lighten_segments
