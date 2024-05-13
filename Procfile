a9a1ea951ef3754c8466a96d943b30ee11ece076
Quentin <quentinpayet1203@gmail.com>
2024-05-13 23:05:11 +0400

    .env.dev and .env.prod files added

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: uvicorn --host=0.0.0.0 --port=${PORT:-5000} --ws-max-size 100000000 main:app
+web: uvicorn --host=0.0.0.0 --port=${PORT:-5000} --ws-max-size 100000000 main:app --env-file ./.env.prod
 # --ws-max-size 100000000 file size limit is set to 100Mb
 # Old => web: fastapi run --host=0.0.0.0 --port=${PORT:-5000}
--- source/utils/websocket_utils.py
+++ source/utils/websocket_utils.py
@@ -5,16 +5,7 @@ from fastapi import WebSocket
 # TODO: Find a cleaner way to do
 # TODO: Specify front prod url in .env.prod file
 def is_connection_authorized(websocket: WebSocket) -> bool:
-    if not (
-        (
-            os.environ.get("DEVMODE") == 1
-            and websocket.headers.get("origin") == "http://localhost:3000"
-        )
-        or (
-            websocket.headers.get("origin")
-            == "https://s11-front-f89f0835e50e.herokuapp.com"
-        )
-    ):
+    if websocket.headers.get("origin") == os.environ.get("FRONTURL"):
         return True
     else:
         print("Unauthorized access => ", websocket.headers.get("origin"))
