4594866904817d6069485740451a0c1d4e64f9ca
Quentin <quentinpayet1203@gmail.com>
2024-06-03 16:47:11 +0400

    attempt to add newer front URLs

--- source/utils/websocket_utils.py
+++ source/utils/websocket_utils.py
@@ -4,7 +4,7 @@ from fastapi import WebSocket
 
 # TODO: Find a cleaner way to do
 def is_connection_authorized(websocket: WebSocket) -> bool:
-    if websocket.headers.get("origin") == os.environ.get("FRONTURL"):
+    if websocket.headers.get("origin") in os.environ.get("FRONTURLS").split(" "):
         return True
     else:
         print("Unauthorized access => ", websocket.headers.get("origin"))
